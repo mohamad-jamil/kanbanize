3f78a92ab4bca6e40380a5c60c95905cbec5b5ef
mohamad-jamil <mohamadjamil03@hotmail.com>
2024-09-11 19:44:42 +0100

    Add button component for adding a new card

---
+++ src/components/AddButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AddButton() {
+  return (
+    <button
+      type="button"
+      className="btn btn-outline-light position-absolute top-0 end-0 mt-3 me-3"
+    >
+      + Add
+    </button>
+  );
+}
+
+export default AddButton;
